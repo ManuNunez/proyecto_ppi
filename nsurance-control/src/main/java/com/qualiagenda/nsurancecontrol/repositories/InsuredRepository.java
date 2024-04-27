dd0c98073364223ed9e9c6896112f8c64c8e99a5
MNunez <manu.nunez.2004@gmail.com>
2024-04-26 18:25:18 -0600

    feat (repositories): InsuredRepositorie.java added

---
+++ nsurance-control/src/main/java/com/qualiagenda/nsurancecontrol/repositories/InsuredRepository.java
@@ -0,0 +1,13 @@
+package com.qualiagenda.nsurancecontrol.repositories;
+
+import com.qualiagenda.nsurancecontrol.models.Insured;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Repository
+public interface InsuredRepository extends CrudRepository<Insured, Long>{
+    public Optional<ArrayList<Insured>> FindInsuredByName(String name);
+}
