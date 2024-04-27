13e5fea0ce0463fb9bdda5e5404c9a1dd566a8f6
MNunez <manu.nunez.2004@gmail.com>
2024-04-26 18:24:43 -0600

    feat (models): Insured.java added

---
+++ nsurance-control/src/main/java/com/qualiagenda/nsurancecontrol/models/Insured.java
@@ -0,0 +1,35 @@
+package com.qualiagenda.nsurancecontrol.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "insureds")
+public class Insured {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String phone;
+
+    public Long getID() {
+        return id;
+    }
+    public void setID(Long ID) {
+        id = ID;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String Name){
+        name = Name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String Phone){
+        phone = Phone;
+    }
+
+}
