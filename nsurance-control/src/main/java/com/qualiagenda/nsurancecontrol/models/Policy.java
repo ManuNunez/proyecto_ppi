0a9e0ece590444796b612524fc47361d3dd0ab28
MNunez <manu.nunez.2004@gmail.com>
2024-04-26 18:24:22 -0600

    feat (models): Policy.java added

---
+++ nsurance-control/src/main/java/com/qualiagenda/nsurancecontrol/models/Policy.java
@@ -0,0 +1,49 @@
+package com.qualiagenda.nsurancecontrol.models;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "policies")
+public class Policy {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "policy_number")
+    private String policyNumber;
+    
+    @ManyToOne
+    @JoinColumn(name = "insured_id", nullable = false)
+    private Insured insured;
+    
+    @Column(name = "insured_vehicle")
+    private String insuredVehicle;
+    
+    @Column(name = "policy_type")
+    private String policyType;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long ID) {
+        this.id = ID;
+    }
+    public Insured getInsured() {
+        return insured;
+    }
+    public void setInsured(Insured INSURED) {
+        this.insured = INSURED;
+    }
+    public String getInsuredVehicle() {
+        return insuredVehicle;
+    }
+    public void setInsuredVehicle(String INSUREDVEHICLE) {
+        this.insuredVehicle = INSUREDVEHICLE;
+    }
+    public String getPolicyType(){
+        return policyType;
+    }
+    public void setPolicyType(String POLICYTYPE){
+        this.policyType = POLICYTYPE;
+    }
+}
