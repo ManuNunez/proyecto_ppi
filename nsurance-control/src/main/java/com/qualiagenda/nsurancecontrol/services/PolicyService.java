e6fe0a7e191f2bf7634379eed74304b3486d8a6a
MNunez <manu.nunez.2004@gmail.com>
2024-04-27 13:11:25 -0600

    feat (Policy) : Curd services

--- nsurance-control/src/main/java/com/qualiagenda/nsurancecontrol/services/PolicyService.java
+++ nsurance-control/src/main/java/com/qualiagenda/nsurancecontrol/services/PolicyService.java
@@ -1,5 +1,46 @@
 package com.qualiagenda.nsurancecontrol.services;
 
+import com.qualiagenda.nsurancecontrol.models.Policy;
+import com.qualiagenda.nsurancecontrol.repositories.PolicyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
 public class PolicyService {
+    @Autowired
+    PolicyRepository repository;
+
+    public Policy savePolicy(Policy policy){
+        String policyNumber = policy.getPolicyNumber();
+        if (repository.findPolicyByPolicyNumber(policyNumber).isEmpty()){
+            return repository.save(policy);
+        }
+        else {
+            Policy policyError = new Policy();
+            policyError.setId(-1L);
+            return policyError;
+        }
+    }
+    public ArrayList<Policy> getAllPolicies() {
+        return (ArrayList<Policy>) repository.findAll();
+    }
+    public Optional<Policy> findPolicyById(Long ID){
+        return repository.findPolicyById(ID);
+    }
+    public String deletePolicyById(Long ID){
+        if (findPolicyById(ID).isPresent()) {
+        repository.deleteById(ID);
+        return "Policy deleted";
+        }
+        else{
+            return "Policy not found";
+        }
+    }
+    public Policy editPolicy(Policy policy) {
+        return repository.save(policy);
+    }
 
 }
